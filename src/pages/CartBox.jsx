e5772d0978009845a1f8533e4d69e6f38385a22c
sk Lee <znfnfn2140@gmail.com>
2022-12-01 16:16:15 +0900

    장바구니 테스트

--- src/pages/CartBox.jsx
+++ src/pages/CartBox.jsx
@@ -1,8 +1,32 @@
 import "../css/cartStyle.css";
 import { useEffect, useState } from "react";
-import Cartlist from "../components/Cartlist";
+import CartItem from "../components/CartItem";
 
 const CartBox = () => {
+  // 임시데이터 - 상품리스트(json)
+  const [product, setProduct] = useState([
+    {
+      productID: 1,
+      category: "Short Sleeve T-shirt",
+      productName: "standard-fit",
+      price: 9500,
+      productImg: "상품이미지1",
+    },
+    {
+      productID: 2,
+      category: "Short Sleeve T-shirt",
+      productName: "slim-fit",
+      price: 9000,
+      productImg: "상품이미지2",
+    },
+    {
+      productID: 3,
+      category: "long Sleeve T-shirt",
+      productName: "standard-fit",
+      price: 10000,
+      productImg: "상품이미지3",
+    },
+  ]);
   // 임시데이터 - 장바구니(로컬스토리지)
   const [list, setList] = useState([
     {
@@ -11,7 +35,7 @@ const CartBox = () => {
       color: "black",
       size: "S",
       print: "front",
-      quantity: 5,
+      amount: 5,
       pay: 47500,
     },
     {
@@ -20,7 +44,7 @@ const CartBox = () => {
       color: "navy",
       size: "M",
       print: "back",
-      quantity: 2,
+      amount: 2,
       pay: 18000,
     },
     {
@@ -29,35 +53,93 @@ const CartBox = () => {
       color: "white",
       size: "L",
       print: "front / back",
-      quantity: 3,
+      amount: 3,
       pay: 30000,
     },
   ]);
   // 상품상세페이지(구매/장바구니 버튼)에서 로컬스토리지에 장바구니 데이터를 담았다고 가정
-  // 로컬스토리지 확인 : 개발자도구(f12) > Application > Local Storage
-  // JSON.stringify() : 문자열 데이터로 저장하기 위함
-  //                    문자열이 아닌채로 로컬스트리지에 데이터를 넣으면 [Object Object Object]로 들어감
+  // 로컬스토리지에 값이 들어간 것을 확인 후 주석처리 할 것
   // localStorage.setItem("moti_cartlist", JSON.stringify(list));
 
   // 로컬스토리지의 장바구니 데이터 들고오기
   const [cartlist, setCartlist] = useState([]);
+  const [deliveryPay, setDeliveryPay] = useState(3000);
+  // 대량 구매 할인을 할 것인지?
+  // 얼마 이상 배송비 무료 할 것인지?
+  // 숫자(금액) 세자리 마다 콤마 넣기
 
   useEffect(() => {
     const data = localStorage.getItem("moti_cartlist");
     setCartlist(data ? JSON.parse(data) : []);
   }, []);
 
+  // 상품리스트에서 cartlist의 상품정보 찾기
+  const findProduct = (i) => {
+    return product.find((item) => item.productID == i.productID);
+  };
+
+  // 상품 총 금액 (배송비 제외), 총 구매수량
+  const productTotal = () => {
+    // prev: 이전값 > 현재까지 누적된 값
+    // cur : 현재값
+    // 0 : 초기값, 안쓰면 배열의 첫번째 요소가 들어감
+    const totalPay = cartlist.reduce((prev, cur) => {
+      return (prev += findProduct(cur).price * cur.amount);
+    }, 0);
+    const totalAmount = cartlist.reduce((prev, cur) => {
+      return (prev += cur.amount);
+    }, 0);
+    return { pay: totalPay, amount: totalAmount };
+  };
+
+  // 상품 총 금액 (배송비 포함)
+  const totalPay = () => {
+    return productTotal().pay + deliveryPay;
+  };
+
+  // 장바구니 비우기
+  const emptyCart = () => {
+    setCartlist([]);
+    setDeliveryPay(0);
+    localStorage.removeItem("moti_cartlist");
+  };
+
   return (
     <div>
       <h1>장바구니</h1>
       <div>상품정보 ㅣ 사이즈 ㅣ 구매수량 ㅣ 상품금액</div>
-      <hr />
-      <Cartlist cartlist={cartlist} setCartlist={setCartlist} />
-      <hr />
-      <div>상품금액 : 0000</div>
-      <div>배송비 : 0000</div>
-      <div>결제금액 : 0000</div>
+      {cartlist.length == 0 ? (
+        <div className="cartlist-wrap">장바구니가 비어있습니다</div>
+      ) : (
+        <div className="cartlist-wrap">
+          {cartlist.map((item) => (
+            <CartItem
+              key={item.cartID}
+              item={item}
+              cartlist={cartlist}
+              setCartlist={setCartlist}
+              findProduct={findProduct}
+            />
+          ))}
+        </div>
+      )}
+      <h3>총수량 : {productTotal().amount}</h3>
+      <h3>상품금액 : {productTotal().pay}</h3>
+      <h3>배송비 : {deliveryPay}</h3>
+      <h3>결제예상금액 : {totalPay()}</h3>
+      <div className="cartlist-wrap">
+        <h2>배송지 정보</h2>
+        배송지 직접 입력하는 공간 <br />
+        저장된 배송지 정보 불러오는 버튼 <br />
+      </div>
       <button>주문하기</button>
+      <button onClick={emptyCart}>장바구니 비우기</button>
+      <div>
+        <br />
+        주문하기 버튼을 누르면 모달창으로 주문완료 띄우기, 데이터는 orderlist로 이동 <br />
+        주문완료 모달창에는 홈으로 가기/주문내역 확인하기 버튼 <br />
+        홈은 홈으로, 주문내역은 마이페이지로 이동
+      </div>
     </div>
   );
 };
