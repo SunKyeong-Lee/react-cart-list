cd151664b496b81940cd9b7fcb8e134ed6cc64cf
sk Lee <znfnfn2140@gmail.com>
2023-01-06 18:05:44 +0900

    네브바

---
+++ src/components/NavbarComp.jsx
@@ -0,0 +1,39 @@
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const NavbarComp = () => {
+  return (
+    <MyNavbar bg="dark" variant="dark">
+      <Nav className="me-auto">
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "active" : undefined)}
+        >
+          Product
+        </NavLink>
+        <NavLink to="/cart">Cart</NavLink>
+      </Nav>
+    </MyNavbar>
+  );
+};
+
+export default NavbarComp;
+
+const MyNavbar = styled(Navbar)`
+  padding: 1rem 1.5rem;
+  a {
+    text-decoration: none;
+    color: #e2e2e2;
+    font-size: 1.5rem;
+    font-weight: 100;
+    &:first-child {
+      margin-right: 2rem;
+    }
+    &.active {
+      color: white;
+      font-weight: 500;
+    }
+  }
+`;
